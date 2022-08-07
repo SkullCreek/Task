de2f35c9529d889e782b40878430aa109dcc171c
Darpanarayan Bahadur <darpan.bahadur.2001@gmail.com>
2022-08-07 18:31:18 +0530

    responsive

--- src/script/script.js
+++ src/script/script.js
@@ -1,1 +1,11 @@
 // script
+const hamburger_button = document.getElementById("hamburger");
+const mobile_menu = document.getElementById("mobile-menu");
+hamburger_button.onclick = () => {
+    mobile_menu.style.display = "grid";
+}
+
+const crossbtn = document.getElementById("cross-btn");
+crossbtn.onclick = () => {
+    mobile_menu.style.display = "none";
+}
